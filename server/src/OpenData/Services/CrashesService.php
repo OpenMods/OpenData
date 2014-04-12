da311cd657ce7115b94a48309aa03158acdc3bcc
Mikee Franklin <mikeefranklin@gmail.com>
2014-04-12 20:33:00 +0100

    derple

--- server/src/OpenData/Services/CrashesService.php
+++ server/src/OpenData/Services/CrashesService.php
@@ -5,8 +5,6 @@ namespace OpenData\Services;
 class CrashesService extends BaseService {
 
     public function add($packet) {
-
-        $packet['date'] = new \MongoDate($packet['date']->format('U'));
         $packet['stackhash'] = md5(serialize($packet['stacktrace']));
         $this->db->crashes->insert($packet);
     }
