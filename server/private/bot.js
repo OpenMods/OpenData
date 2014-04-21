6bc77ac91f40a06b9a9977d7be7a7e1b081b0c44
Mikee Franklin <mikeefranklin@gmail.com>
2014-04-21 21:29:55 +0100

    switched to a node based irc bot for the time being

---
+++ server/private/bot.js
@@ -0,0 +1,195 @@
+String.prototype.startsWith = function (str){
+	return this.slice(0, str.length) == str;
+};
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+Array.prototype.unique = function() {
+    var a = this.concat();
+    for(var i=0; i<a.length; ++i) {
+        for(var j=i+1; j<a.length; ++j) {
+            if(a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+};
+
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+if (process.args.length != 4) {
+	process.exit();
+}
+
+var dbUser = process.args[2];
+var dbPass = process.args[3];
+
+
+var myNick = 'OpenEye2';
+var mainChannel = '#OpenEye';
+var connectionString = 'mongodb://' + dbUser + ':' + dbPass + '@openeye.openmods.info:27017/hopper';
+
+var MongoClient = require('mongodb').MongoClient
+var irc = require('irc');
+var mods  = require('./mods.js');
+var files  = require('./files.js');
+
+var actions = {
+	'mod:find': {
+		func: mods.findMods,
+		secure: false
+	},
+	'mod:get' : {
+		func: mods.getFields,
+		secure: false
+	},
+	'mod:update' : {
+		func: mods.setField,
+		secure: true
+	},
+	'mod:unset' : {
+		func: mods.unsetField,
+		secure: true
+	},
+	'mod:admins:add' : {
+		func: mods.addAdmin,
+		secure: true
+	},
+	'mod:admins:remove' : {
+		func: mods.removeAdmin,
+		secure: true
+	},
+	'file:notes:add' : {
+		func: files.addNote,
+		secure: true
+	},
+	'file:notes:list' : {
+		func: files.listNotes,
+		secure: false
+	},
+	'file:notes:remove' : {
+		func: files.removeNote,
+		secure: true
+	},
+	'commands': {
+		func: function(context) {
+			var keys = [];
+			for (key in actions) {
+				keys.push(key);	
+			}
+			context.bot.say(context.channel, keys.join(', '));
+		},
+		secure: false	
+	}
+};
+
+
+MongoClient.connect(connectionString, function(err, db) {
+
+	var bot = new irc.Client('irc.esper.net', myNick, {
+		channels: [mainChannel],
+		debug: true
+	});
+
+	
+	bot.addListener('message', function (from, to, message) {
+
+	    if (message.startsWith('!')) {
+	    
+		var matches = message.match(/^\!([a-z0-9\:]+)\s?(.*)?$/i);
+
+		if (matches.length > 0) {
+			
+			var action = actions[matches[1]];
+		
+			if (action != null) {
+
+				var tmp = [];
+				if (matches[2] != null) {
+					tmp = matches[2].match(/('(\\'|[^'])*'|"(\\"|[^"])*"|\/(\\\/|[^\/])*\/|(\\ |[^ ])+|[\w-]+)/g);
+				}
+				
+				if (tmp == null) {
+					tmp = [];
+				}
+
+				var args = [];
+
+				for (var i = 0; i < tmp.length; i++) {
+					if ((tmp[i].startsWith("\"") && tmp[i].endsWith("\"")) ||
+					    (tmp[i].startsWith("'") && tmp[i].endsWith("'"))) {
+						args.push(tmp[i].substring(1, tmp[i].length - 1));
+					} else {
+						args.push(tmp[i]);
+					}
+				}
+				
+				
+				
+				if (action.secure) {
+					
+					console.log('secure action');
+					
+					console.log('Checking ' + from);
+				
+					bot.whois(from, function(info) {
+					
+						var username = info['account'];
+
+						if (username == null) {
+							bot.say(from, 'Not authorized');
+							return;
+						}
+						
+						var isOp = info['channels'].indexOf('@#OpenEye') > -1 ||
+							info['channels'].indexOf('@+#OpenEye') > -1;
+														
+						action['func']({
+							bot: bot,
+							db: db,
+							username: username,
+							isOp: isOp,
+							from: from,
+							channel: to,
+							args: args
+						});
+
+					});
+				} else {
+				
+					action['func']({
+						bot: bot,
+						db: db,
+						username: null,
+						isOp: false,
+						from: from,
+						channel: to,
+						args: args
+					});
+				}
+			
+			}
+		}
+		
+
+		console.log(from + ' => ' + to + ': ' + message);
+
+	    }
+	});
+
+	bot.addListener('error', function (e) {
+		console.log(e);
+	});
+});
---
+++ server/private/mods.js
@@ -0,0 +1,369 @@
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+
+
+var validFields = [
+	'name', 'description', 'url', 'parent',
+	'donation', 'authors', 'releasesPage', 'credits',
+	'tags', 'repository', 'irc', 'credits', 'admins'
+];
+
+
+function findMods(context) {
+	
+	var args = context.args;
+	
+	if (args.length != 1) {
+		context.bot.say(context.channel, 'Invalid number of arguments. Expected 1');
+		return;
+	}
+
+	context.db.collection('mods').find(
+		{_id: new RegExp(args[0], 'i')}
+	).toArray(function(err, results) {
+		var response = [];
+		var i =0;
+		results.forEach(function(result) {
+			if (i < 50) {
+				response.push(result.name + ' (' + result._id + ')');
+			}
+			i++;
+		});
+		if (response.length > 0) {
+			var msg = i + ' results found.';
+			if (i > 50) {
+				msg = msg + ' Limiting to 50!';
+			}
+			context.bot.say(context.channel, msg);
+		}
+		context.bot.say(context.channel, response.join(', '));
+	});
+}
+
+function removeAdmin(context) {
+
+	var args = context.args;
+	
+	if (!context.isOp) {
+		context.bot.say(context.channel, 'Sorry, but no.');
+		return;
+	}
+	
+	if (args.length == 2) {
+		
+		var modId = args[0];
+		var username = args[1];
+		
+		getMod(context, modId, false, function(mod) {
+		
+			var admins = [];
+			if (mod['admins'] != null) {
+				admins = mod['admins'];
+			}
+			admins.remove(username);
+			
+			context.db.collection('mods').update(
+				{'_id': modId},
+				{'$set': {
+					'admins' : admins
+				}},
+				{},
+				function(err) {
+					if (err) {
+						context.bot.say(
+							context.channel,
+							err.message
+						);
+						return;
+					}
+					context.bot.say(
+						context.channel,
+						'Update successful'
+					);
+				}
+
+			);
+		
+		});
+		
+	} else {
+		context.bot.say(context.channel, 'Bad usage');
+	}
+}
+
+function addAdmin(context) {
+
+	var args = context.args;
+	
+	if (!context.isOp) {
+		context.bot.say(context.channel, 'Sorry, but no.');
+		return;
+	}
+	
+	if (args.length == 2) {
+		
+		var modId = args[0];
+		var username = args[1];
+		
+		getMod(context, modId, false, function(mod) {
+		
+			var admins = [];
+			if (mod['admins'] != null) {
+				admins = mod['admins'];
+			}
+			if (admins.indexOf(username) == -1) {
+				admins.push(username);
+			}
+			
+			context.db.collection('mods').update(
+				{'_id': modId},
+				{'$set': {
+					'admins' : admins
+				}},
+				{},
+				function(err) {
+					if (err) {
+						context.bot.say(
+							context.channel,
+							err.message
+						);
+						return;
+					}
+					context.bot.say(
+						context.channel,
+						'Update successful'
+					);
+				}
+
+			);
+		
+		});
+		
+	} else {
+		context.bot.say(context.channel, 'Bad usage');
+	}
+}
+
+function getMod(context, modId, requiresPermissions, callback) {
+	context.db.collection('mods').findOne(
+		{_id : modId},
+		function(err, result){ 
+			if (result == null) {
+				context.bot.say(
+					context.channel,
+					'Mod not found'
+				);
+				return;
+			}
+			if (requiresPermissions && !context.isOp) {
+				if (result['admins'] == null || result['admins'].indexOf(context.username) == -1) {
+					context.bot.say(
+						context.channel,
+						'Not authorized'
+					);
+					return;
+				}
+			}
+			callback(result);
+		}
+	);
+}
+
+function setField(context) {
+	
+	if (context.args.length >= 3) {
+		
+		var modId = context.args[0];
+		var field = context.args[1];
+		var value = context.args[2];
+		
+		if (field == 'admins') {
+			context.bot.say(context.channel, 'Use mod:admins:add or mod:admins:remove');
+			return;
+		}
+		
+		context.args.shift();
+		context.args.shift();
+		
+		if (field == 'irc' && context.args.length != 2) {
+			context.bot.say(context.channel, 'Not enough arguments');
+			return;
+		}
+		
+		if (validFields.indexOf(field) == -1) {
+			context.bot.say(
+				context.channel,
+				'Invalid field'
+			);
+			return;
+		}
+		
+		getMod(context, modId, true, function(mod) {
+		
+			if (field == 'irc') {
+				value = {'host' : context.args[0], 'channel' : context.args[1]};
+			} else if (field == 'authors' || field == 'tags') {
+				value = context.args;
+			}
+
+			var set = {};
+			set[field] = value;
+
+			context.db.collection('mods').update(
+				{'_id': modId},
+				{'$set': set},
+				{},
+				function(err) {
+					if (err) {
+						context.bot.say(
+							context.channel,
+							err.message
+						);
+						return;
+					}
+					context.bot.say(
+						context.channel,
+						'Update successful'
+					);
+				}
+
+			);
+		
+		});
+	} else {
+		context.bot.say(context.channel, 'Bad usage');
+	}
+
+}
+
+function unsetField(bot, db, username, nickname, isOp, channel, args) {
+	
+	if (context.args.length == 2) {
+		
+		var modId = context.args[0];
+		var field = context.args[1];
+		
+				
+		if (validFields.indexOf(field) == -1) {
+			context.bot.say(
+				context.channel,
+				'Invalid field'
+			);
+			return;
+		}
+		
+		getMod(context, modId, true, function(mod) {
+				
+			var action = {};
+			var tmp = {};
+
+			if (field == 'tags' || field == 'authors') {
+				tmp[field] = [];
+				action['$set'] = tmp;
+			} else if (field == 'irc') {
+				tmp[field] = 1;
+				action['$unset'] = tmp;
+			} else {
+				tmp[field] = '';
+				action['$set'] = tmp;
+			}
+
+
+			context.db.collection('mods').update(
+				{'_id': modId},
+				action,
+				{},
+				function(err) {
+					if (err) {
+						context.bot.say(
+							context.channel,
+							err.message
+						);
+						return;
+					}
+					context.bot.say(
+						context.channel,
+						'Update successful'
+					);
+				}
+
+			);
+		});
+	} else {
+		context.bot.say(context.channel, 'Bad usage');
+	}
+
+}
+
+function getFields(context) {
+
+	if (context.args.length > 0) {
+		
+		var modId = context.args[0];
+		context.args.shift();
+		var fields = context.args;
+		
+		getMod(context, modId, false, function(mod) {
+				
+			fields.forEach(function(field) {
+
+				if (validFields.indexOf(field) == -1) {
+
+					context.bot.say(
+						context.channel,
+						'Invalid field: '+field
+					);
+
+				} else {
+
+					if (mod[field] === undefined || mod[field] === null) {
+						context.bot.say(
+							context.channel,
+							'Field not found: '+field
+						);
+					} else {
+						var fieldData = mod[field];
+												
+						if (field == 'irc') {
+							context.bot.say(
+								context.channel,
+								'irc: ' + fieldData.host + ' ' + fieldData.channel
+							);
+						} else if (Array.isArray(fieldData)) {
+							context.bot.say(
+								context.channel,
+								field + ': ' + fieldData.join(' ')
+							);
+						} else {
+							context.bot.say(
+								context.channel,
+								field + ': ' + fieldData
+							);
+						}
+					}
+				}
+			});
+		});
+	
+	
+	} else {
+		context.bot.say(context.channel, 'Bad usage');
+	}
+
+}
+
+module.exports.findMods = findMods;
+module.exports.getFields = getFields;
+module.exports.setField = setField;
+module.exports.unsetField = unsetField;
+module.exports.addAdmin = addAdmin;
+module.exports.removeAdmin = removeAdmin;
