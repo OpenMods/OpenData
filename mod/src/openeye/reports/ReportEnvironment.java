2a233348731d15ac27edfe752199c1aad052d20b
Bartek Bok <bartek.bok@gmail.com>
2014-05-09 23:41:55 +0200

    Fix derp

--- mod/src/openeye/reports/ReportEnvironment.java
+++ mod/src/openeye/reports/ReportEnvironment.java
@@ -1,30 +1,18 @@
 package openeye.reports;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.gson.annotations.SerializedName;
 
 public abstract class ReportEnvironment implements IReport {
 
-	public interface RuntimeMeta {}
-
-	public static class FmlForgeRuntime implements RuntimeMeta {
-		@SerializedName("fml")
-		public String fmlVersion;
-
-		@SerializedName("forge")
-		public String forgeVersion;
-
-		@SerializedName("mcp")
-		public String mcpVersion;
-	}
-
 	@SerializedName("branding")
 	public List<String> branding;
 
 	@SerializedName("runtime")
-	public RuntimeMeta runtime;
+	public Map<String, String> runtime;
 
 	@SerializedName("minecraft")
 	public String minecraft;
