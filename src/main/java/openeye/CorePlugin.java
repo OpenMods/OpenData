a62ab514a5f6fa42e9822af38ebb01b5006f594f
Bartek Bok <bartek.bok@gmail.com>
2016-05-03 19:19:26 +0200

    Merge few classes

--- src/main/java/openeye/CorePlugin.java
+++ src/main/java/openeye/CorePlugin.java
@@ -1,15 +1,38 @@
 package openeye;
 
+import java.util.Map;
+
+import openeye.logic.Bootstrap;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 @Name("OpenEyePlugin")
 @TransformerExclusions("openeye")
-public class CorePlugin extends CorePluginBase {
+public class CorePlugin implements IFMLLoadingPlugin {
 
 	@Override
 	public String getAccessTransformerClass() {
 		return null;
 	}
 
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[] { "openeye.asm.MultiTransformer" };
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return "openeye.Mod";
+	}
+
+	@Override
+	public String getSetupClass() {
+		return "openeye.SetupHook";
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+		Bootstrap.instance.populateFromInject(data);
+	}
 }
